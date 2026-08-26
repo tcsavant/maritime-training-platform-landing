739a9b6dc2b17e7ea427203f78db8d0d540fd80a
pavlo-tcsavant <309482088+pavlo-tcsavant@users.noreply.github.com>
2026-08-26 23:31:54 +0300

    Split theme styles into shared and page bundles

--- wp-theme/auvenda-theme/inc/assets.php
+++ wp-theme/auvenda-theme/inc/assets.php
@@ -7,13 +7,17 @@ function auvenda_enqueue_assets() {
     $is_catalogue = is_page_template('page-catalogue.php') || is_post_type_archive('course') || is_tax('course_direction');
     $is_service = is_404() || is_page_template('page-maintenance.php') || (function_exists('auvenda_should_show_maintenance') && auvenda_should_show_maintenance());
 
-    if ($is_catalogue) {
-        wp_enqueue_style('auvenda-catalogue-design', $uri . '/styles.css', array(), filemtime($dir . '/styles.css'));
+    if ($is_service) {
+        $page_handle = 'auvenda-service-page';
+        wp_enqueue_style($page_handle, $uri . '/assets/css/service-pages.css', array(), filemtime($dir . '/assets/css/service-pages.css'));
+    } elseif ($is_catalogue) {
+        $page_handle = 'auvenda-catalogue-page';
+        wp_enqueue_style($page_handle, $uri . '/assets/css/pages/catalogue.css', array(), filemtime($dir . '/assets/css/pages/catalogue.css'));
     } else {
-        wp_enqueue_style('auvenda-landing-design', $uri . '/brand-styles.css', array(), filemtime($dir . '/brand-styles.css'));
-        if ($is_service) wp_enqueue_style('auvenda-service-page', $uri . '/assets/css/service-pages.css', array('auvenda-landing-design'), filemtime($dir . '/assets/css/service-pages.css'));
+        $page_handle = 'auvenda-landing-page';
+        wp_enqueue_style($page_handle, $uri . '/assets/css/pages/landing.css', array(), filemtime($dir . '/assets/css/pages/landing.css'));
     }
-    wp_enqueue_style('auvenda-header', $uri . '/header.css', array(), filemtime($dir . '/header.css'));
+    wp_enqueue_style('auvenda-common', $uri . '/assets/css/common.css', array($page_handle), filemtime($dir . '/assets/css/common.css'));
     wp_enqueue_script('auvenda-main', $uri . '/assets/js/main.js', array(), filemtime($dir . '/assets/js/main.js'), true);
 }
 add_action('wp_enqueue_scripts', 'auvenda_enqueue_assets');
